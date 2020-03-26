34b1019fa8a5113be3d2d7085031b3554cd8d094
James Stamps Jr <jstampshere@gmail.com>
2020-03-26 18:38:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 function takeANumber(current, name) {
     current.length+1;
     current.push(name);
@@ -27,6 +27,73 @@ function currentLine(current) {
     return "The line is currently: " + name.join(", ");
   }
 }  
+*/
+
+
+/*
+function takeANumber(current, name) {
+  current.push(name) && current.length+1;
+    return `Welcome, ${name}. You are number ${current.length} in line.`;
+}
+
+*/
+
+
+
+function takeANumber(current,name) {
+  current.push(name) && current.length+1;
+  for()
+    return `Welcome. You are ticket number ${current[i]}.`;
+}
+
+
+function nowServing(current) {
+  if(current.length < 1) {
+    return "There is nobody waiting to be served!";
+  } else {
+     var inFront = current.shift();
+     return `Currently serving ${inFront}.`;
+  }
+}
+
+function currentLine(current) {
+  if(current.length < 1) {
+    return "The line is currently empty.";
+  } else {
+    var i = 0;
+    var name = [];
+      for(i = 0; i < current.length; i++) {
+        name.push(`${i+1}. ${current[i]}`);
+      }
+      return "The line is currently: " + name.join(", ");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
